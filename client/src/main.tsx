58bac500269288c654e50ef518e44c08d87d8664
sourisagee <serdeshko@icloud.com>
2026-01-08 15:31:07 +0300

    client main tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './app/App';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <>
     <App />
   </>,
