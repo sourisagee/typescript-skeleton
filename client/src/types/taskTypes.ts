f7b9c931aaaca515e2d960443ae5b0dfcff532bc
sourisagee <serdeshko@icloud.com>
2026-01-08 23:12:41 +0300

    client task types

--- client/src/types/taskTypes.ts
+++ client/src/types/taskTypes.ts
@@ -6,3 +6,29 @@ export interface TaskAttributes {
   createdAt?: string;
   updatedAt?: string;
 }
+
+export interface CreateTaskData {
+  title: string;
+  status?: boolean;
+  user_id: number;
+}
+
+export interface UpdateTaskData {
+  title?: string;
+  status?: boolean;
+}
+
+export interface DeleteTaskResponse {
+  deletedCount: number;
+}
+
+export interface TaskResponse {
+  task: TaskAttributes;
+}
+
+export interface ApiResponse<T> {
+  statusCode: number;
+  message: string;
+  data: T | null;
+  error: unknown;
+}
